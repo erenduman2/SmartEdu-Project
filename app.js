f7bff8bc2197c69f2f0b60a33e408f436cdb9d11
erenduman2 <71399989+erenduman2@users.noreply.github.com>
2022-09-19 15:05:21 +0300

    lesson 02

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
+// DK 9
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('index');
+  res.status(200).render('index', {
+    page_name: "index",
+  });
+  // res.status(200).send('index');
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
